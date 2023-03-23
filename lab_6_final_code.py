46c644ad055dd90bd0997805b45b847b6287b979
Amaia Modolo <amaiamodolo@gmail.com>
2023-03-22 23:46:28 -0400

    added the decoder, amaia modolo

--- lab_6_final_code.py
+++ lab_6_final_code.py
@@ -1,3 +1,4 @@
+
 # amaia modolo
 
 '''
@@ -10,10 +11,7 @@ Examples:
 “00009962” will become “33332295” after encoding.
 Decoder
 The password decoder takes in the encoded password and returns the original password. “45678888” needs to
-be decoded back to “12345555” after decoding
-'''
-
-
+be decoded back to “12345555” after decoding'''
 
 def encoder(list):
     temp_list = []
@@ -25,3 +23,42 @@ def encoder(list):
         list = ''.join(temp_list)
     return list
 
+
+
+def decoder(encoded_string):
+    temp_list = []
+    for i in encoded_string:
+        i = int(i)
+        i -= 3
+        i = str(i)
+        temp_list.append(i)
+        decoded_string = ''.join(temp_list)
+    return decoded_string
+
+
+
+def menu():
+    print('Menu\n'
+          '-------------\n'
+          '1. Encode\n'
+          '2. Decode\n'
+          '3. Quit\n')
+
+def main():
+
+    while True:
+        menu()
+        option = int(input('Please enter an option: '))
+        if option == 1:
+            # encode
+            original_str = input('Please enter your password to encode: ')
+            encoded_str = encoder(original_str)
+            print('Your password has been encoded and stored!')
+        if option == 2:
+            decoded_str = decoder(encoded_str)
+            print(f'The encoded password is {encoded_str}, and the original password is {decoded_str}')
+        if option == 3:
+            break
+
+if __name__ == '__main__':
+    main()
